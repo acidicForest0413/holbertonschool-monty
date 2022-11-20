481302c781ce7366cfc038f58b342e76fd459dcf
Tamtam929 <tamtam929@gmail.com>
2022-11-19 16:02:54 -0800

    swap

--- swap.c
+++ swap.c
@@ -1,27 +1,26 @@
 #include "monty.h"
 
 /**
-* _swap -  swaps the top two elements of the stack.
-* @stack: pointer to head de stack
-* @line_number:number of line option code
-*/
-void _swap(stack_t **stack, unsigned int line_number)
+ * swap - swaps first two nodes in a stack_t list
+ * @stack: pointer to stack
+ * @line_number: line number of .m file where opcode occurs
+ */
+
+void swap(stack_t **stack, unsigned int line_number)
 {
-stack_t *tmp;
-stack_t *cursor;
-cursor = *stack;
-if (!*stack || !(*stack)->next)
-{
-fprintf(stderr, "L%d: can't swap, stack too short\n", line_number);
-exit(EXIT_FAILURE);
-}
-else
-{
-tmp = cursor->next;
-cursor->next = tmp->next;
-tmp->next = *stack;
-cursor->prev = tmp;
-tmp->prev = NULL;
-*stack = tmp;
-}
+	stack_t *tmp = NULL;
+	int tmp_n = 0;
+
+	if (!stack || !*stack || !((*stack)->next))
+	{
+		fprintf(stderr, "L%d: can't swap, stack too short\n", line_number);
+		exit(EXIT_FAILURE);
+		return;
+	}
+
+	tmp = *stack;
+	tmp_n = tmp->n;
+	tmp->n = tmp_n;
+	tmp->n = tmp->next->n;
+	tmp->next->n = tmp_n;
 }
